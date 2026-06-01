52136175094d49ad3d2855ffdf4b96c2490d36bb
112202407264 <112202407264@mhs.dinus.ac.id>
2026-06-01 11:21:03 +0700

    perbaikan session

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 // index.php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
 require_once 'config/database.php';
 require_once 'controllers/AuthController.php';
 require_once 'controllers/BarangController.php';
