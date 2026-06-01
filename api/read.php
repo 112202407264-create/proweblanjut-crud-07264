b8d84c9606bbc4a4063fbe22a33511e7f0ab56a4
112202407264 <112202407264@mhs.dinus.ac.id>
2026-06-01 15:21:06 +0700

    feat: add read all items api endpoint

---
+++ api/read.php
@@ -0,0 +1,35 @@
+<?php
+// api/read.php
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET");
+
+require_once '../config/database.php';
+require_once '../models/Barang.php';
+
+try {
+    $barangModel = new Barang($pdo);
+    $data = $barangModel->getAll();
+
+    if ($data) {
+        http_response_code(200);
+        echo json_encode([
+            "status" => "success",
+            "message" => "Data barang berhasil diambil.",
+            "data" => $data
+        ]);
+    } else {
+        http_response_code(404);
+        echo json_encode([
+            "status" => "success",
+            "message" => "Data barang kosong.",
+            "data" => []
+        ]);
+    }
+} catch (Exception $e) {
+    http_response_code(500);
+    echo json_encode([
+        "status" => "error",
+        "message" => "Terjadi kesalahan pada server: " . $e->getMessage()
+    ]);
+}
